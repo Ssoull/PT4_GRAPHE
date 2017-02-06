fb5ebbd16580e2f5e07cd33293ffca9d4a1a1cfe
mleothaud <mathieu.leothaud@etu.u-bordeaux.fr>
2017-02-06 10:42:22 +0100

    Correction de la fonction nulle de Audrey

---
+++ src/sample/Test/AreteTest.java
@@ -0,0 +1,27 @@
+package sample.Test;
+
+import org.junit.Test;
+import sample.Model.Arete;
+import sample.Model.Sommet;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Mathieu on 06/02/2017.
+ */
+public class AreteTest {
+    @Test
+    public void testAjoutArrete() throws Exception {
+        Sommet depart = new Sommet(true);
+        Sommet arriveeF = new Sommet(false);
+        Sommet arrivee = new Sommet(true);
+
+        Arete arete = new Arete(true,depart,arriveeF);
+        assertEquals(arete.id(),-1);
+        assertEquals(arete.valide(),false);
+
+        Arete arete2 = new Arete(true,depart,arrivee);
+        assertEquals(arete2.valide(),true);
+        assertEquals(arete2.id(),0);
+    }
+}
