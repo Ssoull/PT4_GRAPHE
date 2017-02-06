0d28f9ec149ffe3b2eb67f867b37c287a52e4fdb
Lentilhac <audrey.lentilhac@etu.u-bordeaux.fr>
2017-02-06 19:37:51 +0100

    graphe

--- src/sample/Model/Graphe.java
+++ src/sample/Model/Graphe.java
@@ -22,7 +22,6 @@ public class Graphe {
 
     private void lectureGraphe(String fichier, int type){
         String lecture = null;
-
         if (type == 1){
             chargerGrapheDOT(lecture);
         }
@@ -34,8 +33,67 @@ public class Graphe {
 
     }
 
-    private void chargerGrapheGRAPHML(String fichier){
+
+    private void chargerGrapheGRAPHML(String fichier) {
 
     }
 
+    public void ajouterSommet(Sommet s){
+        m_sommets.add(s);
+    }
+
+    public void supprimetSommet(Sommet s){
+        m_sommets.remove(s);
+        m_incidentes.remove(s);
+    }
+
+    public Arete ajouterArete(Sommet n1, Sommet n2){
+        Arete a = new Arete(true, n1, n2);
+        m_aretes.add(a);
+        Pair<Sommet,Sommet> sommets = new Pair<>(n1, n2);
+        m_extremites.put(a, sommets);
+        lierAreteAuSommet(a, n1);
+        lierAreteAuSommet(a, n2);
+        return a;
+    }
+
+    private void lierAreteAuSommet (Arete a, Sommet s){
+        ArrayList<Arete> aretesLocalesSommet = m_incidentes.get(s);
+        if (aretesLocalesSommet.size() == 0){
+            ArrayList<Arete> setArete = new ArrayList<>();
+            setArete.add(a);
+            m_incidentes.put(s, setArete);
+        }
+        else {
+            aretesLocalesSommet.add(a);
+            m_incidentes.put(s, aretesLocalesSommet);
+        }
+    }
+
+    public static ArrayList<Sommet> getM_sommets() { return m_sommets; }
+
+    public static void setM_sommets(ArrayList<Sommet> m_sommets) { Graphe.m_sommets = m_sommets; }
+
+    public static ArrayList<Arete> getM_aretes() { return m_aretes;
+    }
+
+    public static void setM_aretes(ArrayList<Arete> m_aretes) {
+        Graphe.m_aretes = m_aretes;
+    }
+
+    public static HashMap<Sommet, ArrayList<Arete>> getM_incidentes() {
+        return m_incidentes;
+    }
+
+    public static void setM_incidentes(HashMap<Sommet, ArrayList<Arete>> m_incidentes) {
+        Graphe.m_incidentes = m_incidentes;
+    }
+
+    public static HashMap<Arete, Pair<Sommet, Sommet>> getM_extremites() {
+        return m_extremites;
+    }
+
+    public static void setM_extremites(HashMap<Arete, Pair<Sommet, Sommet>> m_extremites) {
+        Graphe.m_extremites = m_extremites;
+    }
 }
